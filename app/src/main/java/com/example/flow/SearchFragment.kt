6a55e82997c391d7f0b7aa52fecce95d558e6579
koreatlwls <koreatlwls@gmail.com>
2023-01-07 15:20:11 +0900

    feat : SearchFragment 추가

---
+++ app/src/main/java/com/example/flow/SearchFragment.kt
@@ -0,0 +1,11 @@
+package com.example.flow
+
+import com.example.flow.databinding.FragmentSearchBinding
+
+class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+    override fun initBinding() {
+        TODO("Not yet implemented")
+    }
+
+}
