0363f3c39384cf0603834528b6a8e8a20c5ad823
koreatlwls <koreatlwls@gmail.com>
2023-01-07 15:27:07 +0900

    feat : Navigation 세팅

--- app/src/main/java/com/example/flow/SearchFragment.kt
+++ app/src/main/java/com/example/flow/SearchFragment.kt
@@ -5,7 +5,7 @@ import com.example.flow.databinding.FragmentSearchBinding
 class SearchFragment : BaseFragment<FragmentSearchBinding>(R.layout.fragment_search) {
 
     override fun initBinding() {
-        TODO("Not yet implemented")
+        binding.lifecycleOwner = viewLifecycleOwner
     }
 
 }
