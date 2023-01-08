4560eda3201e203f8e8fe7d9102cc9540ad7d8fc
koreatlwls <koreatlwls@gmail.com>
2023-01-08 15:08:47 +0900

    feat : 웹뷰 프래그먼트 추가

---
+++ app/src/main/java/com/example/flow/ui/webview/WebViewFragment.kt
@@ -0,0 +1,19 @@
+package com.example.flow.ui.webview
+
+import android.os.Bundle
+import android.view.View
+import com.example.flow.R
+import com.example.flow.databinding.FragmentWebViewBinding
+import com.example.flow.ui.BaseFragment
+
+class WebViewFragment : BaseFragment<FragmentWebViewBinding>(R.layout.fragment_web_view) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+    override fun initBinding() {
+        TODO("Not yet implemented")
+    }
+
+}
